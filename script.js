aa594acde19166a83850b91e8b95f410556d28db
kaif8077 <149231751+kaif8077@users.noreply.github.com>
2025-12-27 00:09:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-const squares = document.querySelectorAll('.square');
+const squares = document.querySelectorAll(".square");
 
 squares.forEach((square) => {
 
-  square.addEventListener('mouseenter', () => {
+  square.addEventListener("mouseenter", () => {
     squares.forEach((s) => {
       if (s !== square) {
-        s.style.backgroundColor = '#6F4E37'; // brown (non-hovered)
-      } else {
-        s.style.backgroundColor = '#E6E6FA'; // hovered stays lavender
+        s.style.backgroundColor = "#6F4E37"; // Coffee
       }
     });
   });
 
-  square.addEventListener('mouseleave', () => {
+  square.addEventListener("mouseleave", () => {
     squares.forEach((s) => {
-      s.style.backgroundColor = '#E6E6FA'; // reset all
+      s.style.backgroundColor = "#E6E6FA"; // Lavender
     });
   });
 
